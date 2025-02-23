b998a5d5ebe2db036d36adaac8758bdd776a0976
Edward Vidal Jr <develone@sbcglobal.net>
2025-02-22 18:48:10 -0700

    commented -    .clk_100p0(pll_clk), +    //.clk_100p0(pll_clk),

--- blnk-ex/ice_makefile_pipelinec/top.sv
+++ blnk-ex/ice_makefile_pipelinec/top.sv
@@ -15,7 +15,7 @@ pll pll_inst(
   .locked()
 );
   pipelinec_top pipelinec_inst(
-    .clk_100p0(pll_clk),
+    //.clk_100p0(pll_clk),
     // RGB LED
     .ice_39_return_output(ICE_39),
     .ice_40_return_output(ICE_40),
