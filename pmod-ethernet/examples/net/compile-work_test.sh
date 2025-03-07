914b38c3bb08ce484835c07667ac2e2066901ef6
Edward Vidal Jr <develone@sbcglobal.net>
2025-03-07 08:19:39 -0700

    gcc -I ../../include -I ../catboard/ice_makefile_pipelinec/pipelinec_output -Wall -pthread work_test.c -o work_test

--- pmod-ethernet/examples/net/compile-work_test.sh
+++ pmod-ethernet/examples/net/compile-work_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- gcc -I ../../include -I ../pico/ice_makefile_pipelinec/pipelinec_output -Wall -pthread work_test.c -o work_test
+ gcc -I ../../include -I ../catboard/ice_makefile_pipelinec/pipelinec_output -Wall -pthread work_test.c -o work_test
